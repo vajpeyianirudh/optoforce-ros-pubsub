9d2f2c7a5044da3ebd7560927bd2814e80357683
vajpeyianirudh <44241666+vajpeyianirudh@users.noreply.github.com>
2020-10-30 01:03:30 -0400

    Add files via upload

--- src/optoforce_sub.cpp
+++ src/optoforce_sub.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
-#include <sstream>
-#include "optoforce_sensor/opto.h"
 #include "ros/ros.h"
 #include "geometry_msgs/WrenchStamped.h"
-#include "std_msgs/String.h"
 #include <string.h>
 
-void subscriberCallback(const std_msgs::String::ConstPtr& msg)
+void subscriberCallback(const geometry_msgs::WrenchStamped::ConstPtr & wrench_msg)
 {
-	//ROS_INFO("%f /n", wrench_msg->wrench.force.x);
-	ROS_INFO("%s", msg->data.c_str());
+	ROS_INFO("%f", wrench_msg->wrench.force.x);
 }
 
 int main(int argc, char **argv)
@@ -19,8 +15,7 @@ int main(int argc, char **argv)
 	ros::NodeHandle n;
 	
 	ros::Subscriber wrench_sub_raw = n.subscribe("OptoForceWrench_raw", 1000, subscriberCallback);
-	ros::Subscriber wrench_sub = n.subscribe("OptoForceWrench", 1000, subscriberCallback);
-	ros::Subscriber wrench_sub_str = n.subscribe("OptoForceWrench_str", 1000, subscriberCallback);
+	ros::subscriber wrench_sub = n.subscriber("OptoForceWrench", 1000, subscriberCallback);
 	
 	ros::spin();
 	
